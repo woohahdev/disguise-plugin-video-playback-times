ecdeed10acce78b9ea3a09108495d7907f013304
Kye Lewis <kye.lewis@woohah.org>
2025-04-08 14:28:56 +1000

    more

--- src/bin.ts
+++ src/bin.ts
@@ -1,20 +1,24 @@
-//import html from "../dist/index.html" with { type: "text " };
-import pkg from "../package.json" with { type: "json" };
+//import pkg from "../package.json" with { type: "json" };
 
 import { createServerAdapter } from "@whatwg-node/server";
 import { createServer, Server } from "node:http";
+import { app, App, BrowserWindow } from "electron";
 
 interface PluginAppOptions {
   name: string;
+  electronApp: App;
 }
 
 class PluginApp {
   server: Server;
   name: string;
   port: number;
+  electronApp: App;
+  window: BrowserWindow;
 
   constructor(options: PluginAppOptions) {
     this.port = 23151;
+
     // You can create your Node server instance by using our adapter
     this.server = createServer(
       createServerAdapter(
@@ -23,12 +27,26 @@ class PluginApp {
     );
 
     this.server.listen(this.port);
-
     this.name = options.name;
     this.port = this.port;
+
+    this.electronApp = options.electronApp;
+
+    console.log(this.electronApp);
+
+    this.window = new BrowserWindow({
+      width: 200,
+      height: 200,
+    });
+
+    this.start();
   }
 
   async start() {
+
+    const html = await fetch("../dist/index.html");
+    console.log(html);
+
     const { MDNS } = await import("@matrixai/mdns");
 
     const mdns = new MDNS();
@@ -45,14 +63,23 @@ class PluginApp {
       type: "d3plugin",
     });
 
+    const url = new URL(`http://${mdns.hostname}:${this.port}`);
     console.log(
-      `Announced Disguise Plugin ${this.name} at http://${mdns.hostname}:${this.port}`,
+      `Announced Disguise Plugin ${this.name} at ${url.toString()}`,
     );
+
+    this.window.loadURL(url.toString());
+
   }
 }
 
+app.whenReady().then(() => {
+
+  console.log("Ready!");
 const pluginApp = new PluginApp({
-  name: pkg.name,
+  name: 'test',//'pkg.name',
+  electronApp: app
 });
 
-pluginApp.start();
+});
+
